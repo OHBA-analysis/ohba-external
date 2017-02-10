f84d839cc518308d9605b689ec17ecf1a82a8c23
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-02-10 11:46:53 +0000

    Added some extra utilities

--- osl_external_startup.m
+++ osl_external_startup.m
@@ -2,3 +2,4 @@
 addpath(fullfile(fileparts(mfilename('fullpath')),'netlab3.3','netlab'))
 addpath(fullfile(fileparts(mfilename('fullpath')),'icasso122'))
 addpath(fullfile(fileparts(mfilename('fullpath')),'FastICA_25'))
+addpath(fullfile(fileparts(mfilename('fullpath')),'ohba_utils'))
