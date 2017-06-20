de7603901420e0c311fd42c8c34478754202887a
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-06-20 08:45:13 +0100

    Added support for removing parcels using a logical vector

--- ohba_utils/@parcellation/remove_parcels.m
+++ ohba_utils/@parcellation/remove_parcels.m
@@ -1,11 +1,24 @@
-function self = remove_parcels(self,indices_to_remove)
+function self = remove_parcels(self,rm)
 	% Remove specified parcels from the parcellation
 	% 
 	% Returns a parcellation with the specified parcels removed
 	%
 	% INPUTS
-	% - Indices of parcels to remove
+	% - Indices of parcels to remove, or logical vector with length equal to n_parcels
+	%
+	% EXAMPLES
+	%
+	% Suppose p is a parcellation object with 3 parcels
+	%
+	% - p2 = p.remove_parcels([1]) - Remove parcel 1
+	% - p2 = p.remove_parcels([1 3]) - Remove parcels 1 and 3
+	% - p2 = p.remove_parcels([1 0 1]) - Remove parcels 1 and 3
 
-	retain = ~ismember(1:self.n_parcels,indices_to_remove);
+	if (islogical(rm) || all(ismember(rm,[0 1]))) && isvector(rm) && length(rm)==self.n_parcels
+		retain = ~logical(rm);
+	else
+		retain = ~ismember(1:self.n_parcels,rm);
+	end
+
 	self.weight_mask = self.weight_mask(:,:,:,retain);
 	self.labels = self.labels(retain);
