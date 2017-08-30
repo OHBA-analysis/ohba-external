f849bd4f08f4ef56d6e5b6b4712c3d4c1f59844f
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2017-08-30 10:29:06 +0100

    OAT still uses pca_netlab

--- ohba_external_startup.m
+++ ohba_external_startup.m
@@ -1,5 +1,5 @@
 % Add libraries in this project to Matlab path
-%addpath(fullfile(fileparts(mfilename('fullpath')),'netlab3.3','netlab'))
+addpath(fullfile(fileparts(mfilename('fullpath')),'netlab3.3','netlab'))
 addpath(fullfile(fileparts(mfilename('fullpath')),'icasso122'))
 addpath(fullfile(fileparts(mfilename('fullpath')),'FastICA_25'))
 addpath(fullfile(fileparts(mfilename('fullpath')),'ohba_utils'))
